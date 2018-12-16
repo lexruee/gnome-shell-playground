7b21b2c496d545c897ad52cc30622d3e29f1a396
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2018-12-16 16:35:37 +0100

    Commit two gjs examples

---
+++ gjs-examples/gettext.js
@@ -0,0 +1,21 @@
+#!/usr/bin/gjs
+
+imports.gi.versions.Gtk = '3.0';
+const Gettext = imports.gettext;
+const Gtk = imports.gi.Gtk;
+
+Gettext.bindtextdomain("gnome-panel-3.0", "/usr/share/locale");
+Gettext.textdomain("gnome-panel-3.0");
+
+Gtk.init(null);
+
+let w = new Gtk.Window({
+    type: Gtk.WindowType.TOPLEVEL,
+    default_height: 200,
+    default_width: 200
+});
+w.connect("delete-event", Gtk.main_quit);
+w.add(new Gtk.Label({ label: Gettext.gettext("Panel") }));
+w.show_all();
+
+Gtk.main();
---
+++ gjs-examples/gio-cat.js
@@ -0,0 +1,30 @@
+#!/usr/bin/gjs
+
+const GLib = imports.gi.GLib;
+const Gio = imports.gi.Gio;
+
+let loop = GLib.MainLoop.new(null, false);
+
+function cat(filename) {
+    let f = Gio.file_new_for_path(filename);
+    f.load_contents_async(null, function(f, res) {
+        let contents;
+        try {
+            contents = f.load_contents_finish(res)[1];
+        } catch (e) {
+            log("*** ERROR: " + e.message);
+            loop.quit();
+            return;
+        }
+        print(contents);
+        loop.quit();
+    });
+
+    loop.run();
+}
+
+if (ARGV.length != 1) {
+    printerr("Usage: gio-cat.js filename");
+} else {
+    cat(ARGV[0]);
+}
